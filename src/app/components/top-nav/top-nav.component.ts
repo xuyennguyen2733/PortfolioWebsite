d9e4fa311731b6bed1cfcf92fa214b3f7b28b52e
THI TRIEU XUYEN NGUYEN <u1252970@umail.utah.edu>
2024-09-20 03:59:46 -0600

    implemented responsive top nav buttons

--- src/app/components/top-nav/top-nav.component.ts
+++ src/app/components/top-nav/top-nav.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
   styleUrl: './top-nav.component.scss',
 })
-export class TopNavComponent {}
+export class TopNavComponent {
+  isExtraSmallScreen: boolean = false;
+  
+  @HostListener('window:resize',['$event'])
+  onResize() {
+    this.isExtraSmallScreen = window.innerWidth < 400;
+  }
+}
